cf3b095c901353511817386977f12853250f5afc
kranks40 <eng_peters20@yahoo.com>
2020-10-07 01:21:48 -0400

    Added movie list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import Row from './components/Row';
+import requests from './utils/request';
 
 
 function App() {
   return (
     <div className="app">
-      <h1>Welcome to Netfix Clone</h1>
+      
+      <Row title='Netflix Originals' fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title='Trending Now' fetchUrl={requests.fetchTrending}/>
+      <Row title='Top Rated' fetchUrl={requests.fetchTrending}/>
+      <Row title='Action Movies' fetchUrl={requests.fetchActionMovies}/>
+      <Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies}/>
+      <Row title='Horror Movies' fetchUrl={requests.fetchHorroryMovies}/>
+      <Row title='Romance Movies' fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries}/>
+
+
     </div>
   );
 }
---
+++ src/utils/axios.js
@@ -0,0 +1,9 @@
+import axios from '.utils/axios';
+
+
+/** base url to make requests to the movie database */
+const instance = axios.create({
+    baseURL: 'https://api.themoviedb.org/3',
+});
+
+export default instance;
